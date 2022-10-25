923b35e013554b9ece8fb33d310562b853774595
Sergios <sergios@138-38-148-59.eduroam.bath.ac.uk>
2022-10-25 10:32:06 +0100

    Add Tennis demo

---
+++ tennis.py
@@ -0,0 +1,14 @@
+import gym
+
+env = gym.make("ALE/Tennis-v5", render_mode="human")
+env.action_space.seed(42)
+
+observation, info = env.reset(seed=42)
+
+for _ in range(1000):
+    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
