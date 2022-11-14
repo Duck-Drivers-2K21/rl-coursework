1c0553fc8b92cc78750cf5446a06a1da7b3f496a
Alex <alexander.dawkins@gmail.com>
2022-11-14 21:06:28 +0000

    naive AI

--- main.py
+++ main.py
@@ -45,7 +45,8 @@ class State:
 
 class Environment:
     def __init__(self, seed: int = 42):
-        env = gym.make("ALE/Pong-v5", render_mode="human", obs_type='ram')
+        env = gym.make("ALE/Pong-v5", render_mode="human", difficulty=0, obs_type='ram', full_action_space=False)
+        # env = gym.make("ALE/Pong-v5", difficulty=0, obs_type='ram', full_action_space=False)
         env.action_space.seed(seed)
         observation, info = env.reset(seed=42)
         self.env = env
@@ -65,8 +66,62 @@ class Environment:
         return self.env.close()
 
 
+tick = False
+prev_bx = 0
+
+
 def get_action(state: State):
-    return Environment.
+    global tick, prev_bx
+
+    px, py = state.player_pos
+    bx, by = state.ball_pos
+    bx, by = int(bx), int(by)
+
+    bv = (bx - prev_bx)
+    b_going_left = bv < 0
+    bs = abs(bv)
+    b_fast = (bs > 4)
+
+    # print(prev_bx, bx, bv, b_fast)
+
+    prev_bx = bx
+
+    if by == 0:
+        if abs(py-80) < 10:
+            return util.Actions.NOOP
+        if py > 80:
+            return util.Actions.RIGHTFIRE
+        else:
+            return util.Actions.LEFTFIRE
+    tick = not tick
+
+    paddle_y_offset = 9
+
+    close_distance = 20
+    big_difference = 12
+    small_difference = 8
+
+    # if b_fast:
+    #     small_difference = 5
+    #     close_distance = 25
+
+    ball_close = (bx >= px - close_distance)
+    py_central = py + paddle_y_offset
+
+    d_y = abs(by - py_central)
+
+    if ((d_y < big_difference and not ball_close)
+            or (d_y < small_difference)):
+        return util.Actions.NOOP
+
+    if d_y > 20 or tick or ball_close:
+        if by < py_central:
+            return util.Actions.RIGHTFIRE
+        else:
+            return util.Actions.LEFTFIRE
+
+    return util.Actions.NOOP
+
 
 def run():
     env = Environment()
@@ -76,7 +131,13 @@ def run():
         while True:
             reward = env.step(action)
             action = get_action(env.state)
-            print(reward)
+            if env.state.terminated:
+                player = env.state.observation[util.MemoryLocations.player_score]
+                enemy = env.state.observation[util.MemoryLocations.enemy_score]
+                win = player > enemy
+                status = "Won" if win else "Lost"
+                print(f"{status}: {player}, {enemy}")
+                break
     except KeyboardInterrupt:
         print("Keyboard Interrupt")
     finally:
