e18d6bee15bd310192fb91212c0ec911306b829b
Alex <alexander.dawkins@gmail.com>
2022-11-21 20:55:38 +0000

    fix typing, get name of agent

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from typing import Type
+
 from Pong import Environment
-from Agents import RandomAgent, NaiveAgent
+from Agents import RandomAgent, NaiveAgent, AbstractAgent
 
 
-def train(agent_type: type, n_episodes: int = 10):
+def train(agent_type: Type[AbstractAgent], n_episodes: int = 10):
     env = Environment()
     agent = agent_type(env)
     try:
@@ -10,7 +12,7 @@ def train(agent_type: type, n_episodes: int = 10):
             episode = agent.generate_episode()
             outcome = "won" if episode.player_score > episode.enemy_score else "lost"
             print(
-                f"Agent {outcome} with a score of {episode.player_score} to {episode.enemy_score}."
+                f"{agent} {outcome} with a score of {episode.player_score} to {episode.enemy_score}."
                 f"({len(episode)} timesteps)"
             )
     except KeyboardInterrupt:
