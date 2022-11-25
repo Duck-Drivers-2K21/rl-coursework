e36e06189cf12fa10afc6fad7b10ea063f11d805
Sergios Gavriilidis <sgavriilidis@hotmail.com>
2022-11-25 14:58:39 +0000

    Update NaiveAgents to use gymnasium Add recording and stats for naive agents Add gitignore

--- Pong.py
+++ Pong.py
@@ -1,4 +1,4 @@
-import gym
+import gymnasium
 
 
 class MemoryLocations:
@@ -72,11 +72,15 @@ class State:
     def is_terminal(self): return self.terminated or self.truncated
 
 
-class Environment:
-    def __init__(self, seed: int = 42, render: bool = False, difficulty: int = 0):
-        render_mode = "human" if render else None
-
-        env = gym.make("ALE/Pong-v5", difficulty=difficulty, obs_type='ram', render_mode=render_mode)
+class NaiveEnvWrapper:
+    """
+    Environment Wrapper for naive agents. Uses RAM represnetation.
+    """
+    def __init__(self, seed: int = 42, render: bool = False, difficulty: int = 0, record: bool = False,
+                 agent_name: str = "NoName"):
+        env = gymnasium.make("ALE/Pong-v5", difficulty=difficulty, obs_type='ram', render_mode="rgb_array")
+        if record:
+            env = gymnasium.wrappers.RecordVideo(env, "videos", episode_trigger=lambda x: x % 100 == 0, name_prefix=agent_name)
 
         env.action_space.seed(seed)
         observation, info = env.reset(seed=seed)
@@ -84,9 +88,8 @@ class Environment:
         self.state = State(observation, info)
 
     def step(self, action: int) -> float:
-        # This will need to be reworked for non-naive agents...
         if self.state.is_terminal:
-            self.reset()  # TODO: Why are we resetting here? Wouldn't an assertion be more logical/safer?
+            assert False, "Terminal state in `step`."
         self.state.observation, reward, self.state.terminated, self.state.truncated, self.state.info = self.env.step(
             action)
         return reward
@@ -95,5 +98,5 @@ class Environment:
         observation, info = self.env.reset()
         self.state = State(observation, info)
 
-    def close(self):
-        return self.env.close()
+    def close(self) -> None:
+        self.env.close()
